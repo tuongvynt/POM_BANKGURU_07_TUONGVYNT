a50c711ed5b106058e16883dac3e022186c0e535
deeelder <nttvy301294@gmail.com>
2019-01-11 19:02:34 +0700

    Create new project PO_BANKGURU_07_TUONGVYNT - By Tuong Vy Nguyen

---
+++ testcases/com/bankguru/account/Account_01_RegisterAndLoginToSystem.java
@@ -0,0 +1,71 @@
+package com.bankguru.account;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Account_01_RegisterAndLoginToSystem {
+  WebDriver driver;
+  private String loginUrl, email, userID, password;
+	
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  
+	  email = "nttvy_" +randomNumber() +"@mailinator.com";
+	  System.out.println("Email = " + email);
+  }
+  
+  
+  @Test
+  public void TC01_RegisterAccount() {
+	  driver.get("http://demo.guru99.com/v4/index.php");
+	  loginUrl = driver.getCurrentUrl();
+	  
+	  driver.findElement(By.xpath("//a[text() ='here']")).click();
+	  Assert.assertTrue(driver.findElement(By.xpath("//input[@name='emailid']")).isDisplayed());
+	 
+	  driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
+	  driver.findElement(By.xpath("//input[@name= 'btnLogin']")).click();
+	  
+	  userID = driver.findElement(By.xpath("//td[text() ='User ID :']/following-sibling::td")).getText();
+	  password = driver.findElement(By.xpath("//td[text() ='Password :']/following-sibling::td")).getText();
+	  
+	  System.out.println("UserID = " + userID);
+	  System.out.println("Password = " + password);
+  }
+  
+  @Test
+  public void TC02_LoginWithCreatedInformation() {
+	  driver.get(loginUrl);
+	  
+	  driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userID);
+	  driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+	  driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+	  
+	  Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()= \"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
+	  Assert.assertTrue(driver.findElement(By.xpath("//td[text() ='Manger Id : "+ userID + "']")).isDisplayed());
+  }
+  
+  public int randomNumber() {
+	  Random random = new Random();
+	  int number = random.nextInt(999999);
+	  return number;
+  }
+  
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
---
+++ testcases/com/bankguru/login/Login_02_ValidateRegisterForm.java
@@ -0,0 +1,39 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_02_ValidateRegisterForm {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+  
+  
+  @Test
+  public void TC01() {
+  }
+  
+  @Test
+  public void TC02() {
+  }
+  
+  @Test
+  public void TC03() {
+  }
+  
+  @Test
+  public void TC04() {
+  }
+  
+  @Test
+  public void TC05() {
+  }
+  
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
